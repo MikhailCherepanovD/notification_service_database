6b339d0d9a084241b18313ab32b81754d4844c60
MikhailCherepanovD <mikhail.cherepanov.2002@mail.ru>
2025-01-21 22:12:04 +0300

    global rename

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,11 @@
-DROP TABLE IF EXISTS passenger  CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS route_monitoring CASCADE ;
-DROP TABLE IF EXISTS passenger_route_monitoring CASCADE;
-DROP FUNCTION IF exists  update_or_insert_passenger;
+DROP TABLE IF EXISTS users_route_monitoring CASCADE;
+DROP FUNCTION IF exists update_or_insert_users;
 
-CREATE TABLE passenger(
-	passenger_id SERIAL PRIMARY KEY,
+
+CREATE TABLE users(
+	users_id SERIAL PRIMARY KEY,
 	login varchar(50),
 	password varchar(100),
 	user_name varchar(50),
@@ -21,18 +22,18 @@ CREATE TABLE route_monitoring(
 	finish_time_monitoring varchar(50)
 );
 
-CREATE TABLE passenger_route_monitoring(
-    passenger_id INT,
+CREATE TABLE users_route_monitoring(
+    users_id INT,
     route_monitoring_id INT,
-	PRIMARY KEY (passenger_id, route_monitoring_id)
+	PRIMARY KEY (users_id, route_monitoring_id)
 );
 
-ALTER TABLE passenger
-ADD CONSTRAINT unique_login UNIQUE (login);
+ALTER TABLE users
+ADD CONSTRAINT unique_login1 UNIQUE (login);
 
 
-CREATE OR REPLACE FUNCTION update_or_insert_passenger( --0 - конфликт на логинах, 1 - значение вставлено, 2 - значение обновлено
-    p_passenger_id INT,
+CREATE OR REPLACE FUNCTION update_or_insert_users( --0 - конфликт на логинах, 1 - значение вставлено, 2 - значение обновлено
+    p_users_id INT,
     p_login TEXT,
     p_password TEXT,
     p_user_name TEXT,
@@ -44,25 +45,25 @@ DECLARE
 BEGIN
 	BEGIN
 	  	IF NOT EXISTS(
-			SELECT 1 FROM passenger WHERE passenger.passenger_id=p_passenger_id
+			SELECT 1 FROM users WHERE users.users_id=p_users_id
 		)
 		THEN 
-			INSERT INTO passenger (passenger_id, login, password, user_name, email, telegram)
-			VALUES (p_passenger_id, p_login, p_password, p_user_name, p_email, p_telegram);
+			INSERT INTO users (users_id, login, password, user_name, email, telegram)
+			VALUES (p_users_id, p_login, p_password, p_user_name, p_email, p_telegram);
 			returned_value = 1;
 		ELSE
-			UPDATE passenger SET 
+			UPDATE users SET 
 				login = p_login,
 				password = p_password,
 				user_name = p_user_name,
 				email = p_email,
 				telegram=p_telegram
-			WHERE passenger_id=p_passenger_id;
+			WHERE users_id=p_users_id;
 			returned_value=2;
 		END IF;
 	EXCEPTION
         WHEN unique_violation THEN
-			returned_value=10;
+			returned_value=0;
 	END;
 	RETURN returned_value;
 END;
